a67b36a629e0d9be280f39fa1f694bbd2d732545
alulab14 <alulab14@inf.pucp.edu.pe>
2018-11-03 11:11:20 -0500

    Se agregaron formularios para la atención a la modificación de Solicitud de requerimientos

---
+++ LP2_Grupo1_Excellia_C#/Vistas/frmRevisarSolicitudRequerimiento.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Vistas
+{
+    partial class frmRevisarSolicitudRequerimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSolicitudModificacionRequerimiento = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblProyecto = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.lblFechaSolicitud = new System.Windows.Forms.Label();
+            this.lblEmpresa = new System.Windows.Forms.Label();
+            this.txtProyecto = new System.Windows.Forms.TextBox();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtEmpresa = new System.Windows.Forms.TextBox();
+            this.txtFechaSolicitud = new System.Windows.Forms.TextBox();
+            this.lblRequerimiento = new System.Windows.Forms.Label();
+            this.txtRequerimiento = new System.Windows.Forms.TextBox();
+            this.lblTipoRequerimiento = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnAprobar = new System.Windows.Forms.Button();
+            this.btnDesaprobar = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblSolicitudModificacionRequerimiento
+            // 
+            this.lblSolicitudModificacionRequerimiento.AutoSize = true;
+            this.lblSolicitudModificacionRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolicitudModificacionRequerimiento.Location = new System.Drawing.Point(238, 49);
+            this.lblSolicitudModificacionRequerimiento.Name = "lblSolicitudModificacionRequerimiento";
+            this.lblSolicitudModificacionRequerimiento.Size = new System.Drawing.Size(417, 25);
+            this.lblSolicitudModificacionRequerimiento.TabIndex = 0;
+            this.lblSolicitudModificacionRequerimiento.Text = "Solicitud de modificación de requerimiento";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDescripcion.Location = new System.Drawing.Point(60, 329);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(80, 16);
+            this.lblDescripcion.TabIndex = 1;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(63, 374);
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(750, 153);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // lblProyecto
+            // 
+            this.lblProyecto.AutoSize = true;
+            this.lblProyecto.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProyecto.Location = new System.Drawing.Point(78, 100);
+            this.lblProyecto.Name = "lblProyecto";
+            this.lblProyecto.Size = new System.Drawing.Size(65, 16);
+            this.lblProyecto.TabIndex = 4;
+            this.lblProyecto.Text = "Proyecto:";
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(78, 134);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(52, 16);
+            this.lblCliente.TabIndex = 5;
+            this.lblCliente.Text = "Cliente:";
+            // 
+            // lblFechaSolicitud
+            // 
+            this.lblFechaSolicitud.AutoSize = true;
+            this.lblFechaSolicitud.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaSolicitud.Location = new System.Drawing.Point(490, 103);
+            this.lblFechaSolicitud.Name = "lblFechaSolicitud";
+            this.lblFechaSolicitud.Size = new System.Drawing.Size(122, 16);
+            this.lblFechaSolicitud.TabIndex = 6;
+            this.lblFechaSolicitud.Text = "Fecha de Solicitud:";
+            // 
+            // lblEmpresa
+            // 
+            this.lblEmpresa.AutoSize = true;
+            this.lblEmpresa.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmpresa.Location = new System.Drawing.Point(490, 136);
+            this.lblEmpresa.Name = "lblEmpresa";
+            this.lblEmpresa.Size = new System.Drawing.Size(66, 16);
+            this.lblEmpresa.TabIndex = 7;
+            this.lblEmpresa.Text = "Empresa:";
+            // 
+            // txtProyecto
+            // 
+            this.txtProyecto.Location = new System.Drawing.Point(149, 99);
+            this.txtProyecto.Name = "txtProyecto";
+            this.txtProyecto.Size = new System.Drawing.Size(205, 20);
+            this.txtProyecto.TabIndex = 8;
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(149, 134);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(205, 20);
+            this.txtCliente.TabIndex = 9;
+            // 
+            // txtEmpresa
+            // 
+            this.txtEmpresa.Location = new System.Drawing.Point(623, 134);
+            this.txtEmpresa.Name = "txtEmpresa";
+            this.txtEmpresa.Size = new System.Drawing.Size(205, 20);
+            this.txtEmpresa.TabIndex = 10;
+            // 
+            // txtFechaSolicitud
+            // 
+            this.txtFechaSolicitud.Location = new System.Drawing.Point(623, 103);
+            this.txtFechaSolicitud.Name = "txtFechaSolicitud";
+            this.txtFechaSolicitud.Size = new System.Drawing.Size(205, 20);
+            this.txtFechaSolicitud.TabIndex = 11;
+            // 
+            // lblRequerimiento
+            // 
+            this.lblRequerimiento.AutoSize = true;
+            this.lblRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRequerimiento.Location = new System.Drawing.Point(78, 179);
+            this.lblRequerimiento.Name = "lblRequerimiento";
+            this.lblRequerimiento.Size = new System.Drawing.Size(99, 16);
+            this.lblRequerimiento.TabIndex = 12;
+            this.lblRequerimiento.Text = "Requerimiento:";
+            // 
+            // txtRequerimiento
+            // 
+            this.txtRequerimiento.Location = new System.Drawing.Point(81, 213);
+            this.txtRequerimiento.Multiline = true;
+            this.txtRequerimiento.Name = "txtRequerimiento";
+            this.txtRequerimiento.Size = new System.Drawing.Size(729, 46);
+            this.txtRequerimiento.TabIndex = 13;
+            // 
+            // lblTipoRequerimiento
+            // 
+            this.lblTipoRequerimiento.AutoSize = true;
+            this.lblTipoRequerimiento.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoRequerimiento.Location = new System.Drawing.Point(89, 284);
+            this.lblTipoRequerimiento.Name = "lblTipoRequerimiento";
+            this.lblTipoRequerimiento.Size = new System.Drawing.Size(149, 16);
+            this.lblTipoRequerimiento.TabIndex = 14;
+            this.lblTipoRequerimiento.Text = "Tipo de Requerimiento:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(244, 284);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // btnAprobar
+            // 
+            this.btnAprobar.Location = new System.Drawing.Point(236, 563);
+            this.btnAprobar.Name = "btnAprobar";
+            this.btnAprobar.Size = new System.Drawing.Size(75, 23);
+            this.btnAprobar.TabIndex = 16;
+            this.btnAprobar.Text = "Aprobar";
+            this.btnAprobar.UseVisualStyleBackColor = true;
+            // 
+            // btnDesaprobar
+            // 
+            this.btnDesaprobar.Location = new System.Drawing.Point(388, 563);
+            this.btnDesaprobar.Name = "btnDesaprobar";
+            this.btnDesaprobar.Size = new System.Drawing.Size(75, 23);
+            this.btnDesaprobar.TabIndex = 17;
+            this.btnDesaprobar.Text = "Desaprobar";
+            this.btnDesaprobar.UseVisualStyleBackColor = true;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Location = new System.Drawing.Point(537, 563);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegresar.TabIndex = 18;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // frmRevisarSolicitudRequerimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 607);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnDesaprobar);
+            this.Controls.Add(this.btnAprobar);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblTipoRequerimiento);
+            this.Controls.Add(this.txtRequerimiento);
+            this.Controls.Add(this.lblRequerimiento);
+            this.Controls.Add(this.txtFechaSolicitud);
+            this.Controls.Add(this.txtEmpresa);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.txtProyecto);
+            this.Controls.Add(this.lblEmpresa);
+            this.Controls.Add(this.lblFechaSolicitud);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.lblProyecto);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblSolicitudModificacionRequerimiento);
+            this.Name = "frmRevisarSolicitudRequerimiento";
+            this.Text = "frmRevisarSolicitudRequerimiento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSolicitudModificacionRequerimiento;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblProyecto;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label lblFechaSolicitud;
+        private System.Windows.Forms.Label lblEmpresa;
+        private System.Windows.Forms.TextBox txtProyecto;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtEmpresa;
+        private System.Windows.Forms.TextBox txtFechaSolicitud;
+        private System.Windows.Forms.Label lblRequerimiento;
+        private System.Windows.Forms.TextBox txtRequerimiento;
+        private System.Windows.Forms.Label lblTipoRequerimiento;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAprobar;
+        private System.Windows.Forms.Button btnDesaprobar;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
---
+++ LP2_Grupo1_Excellia_C#/Vistas/frmSolicitudesModificacionRequerimientos.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Vistas
+{
+    partial class frmSolicitudesModificacionRequerimientos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvSolicitudes = new System.Windows.Forms.DataGridView();
+            this.colProyecto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colEmpresa = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colFechaSolicitud = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblSolicitudesModificacionRequerimientos = new System.Windows.Forms.Label();
+            this.btnRevisarSolicitud = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvSolicitudes
+            // 
+            this.dgvSolicitudes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvSolicitudes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colProyecto,
+            this.colCli,
+            this.colEmpresa,
+            this.colFechaSolicitud});
+            this.dgvSolicitudes.Location = new System.Drawing.Point(86, 104);
+            this.dgvSolicitudes.Name = "dgvSolicitudes";
+            this.dgvSolicitudes.Size = new System.Drawing.Size(742, 336);
+            this.dgvSolicitudes.TabIndex = 0;
+            // 
+            // colProyecto
+            // 
+            this.colProyecto.HeaderText = "Nombre Proyecto";
+            this.colProyecto.Name = "colProyecto";
+            this.colProyecto.Width = 150;
+            // 
+            // colCli
+            // 
+            this.colCli.HeaderText = "Cliente";
+            this.colCli.Name = "colCli";
+            // 
+            // colEmpresa
+            // 
+            this.colEmpresa.HeaderText = "Empresa";
+            this.colEmpresa.Name = "colEmpresa";
+            // 
+            // colFechaSolicitud
+            // 
+            this.colFechaSolicitud.HeaderText = "Fecha de solicitud";
+            this.colFechaSolicitud.Name = "colFechaSolicitud";
+            this.colFechaSolicitud.Width = 150;
+            // 
+            // lblSolicitudesModificacionRequerimientos
+            // 
+            this.lblSolicitudesModificacionRequerimientos.AutoSize = true;
+            this.lblSolicitudesModificacionRequerimientos.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSolicitudesModificacionRequerimientos.Location = new System.Drawing.Point(253, 38);
+            this.lblSolicitudesModificacionRequerimientos.Name = "lblSolicitudesModificacionRequerimientos";
+            this.lblSolicitudesModificacionRequerimientos.Size = new System.Drawing.Size(422, 25);
+            this.lblSolicitudesModificacionRequerimientos.TabIndex = 1;
+            this.lblSolicitudesModificacionRequerimientos.Text = "Solicitudes Modificación de requerimientos";
+            // 
+            // btnRevisarSolicitud
+            // 
+            this.btnRevisarSolicitud.Location = new System.Drawing.Point(721, 75);
+            this.btnRevisarSolicitud.Name = "btnRevisarSolicitud";
+            this.btnRevisarSolicitud.Size = new System.Drawing.Size(98, 23);
+            this.btnRevisarSolicitud.TabIndex = 2;
+            this.btnRevisarSolicitud.Text = "Revisar Solicitud";
+            this.btnRevisarSolicitud.UseVisualStyleBackColor = true;
+            this.btnRevisarSolicitud.Click += new System.EventHandler(this.btnRevisarSolicitud_Click);
+            // 
+            // frmSolicitudesModificacionRequerimientos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(918, 484);
+            this.Controls.Add(this.btnRevisarSolicitud);
+            this.Controls.Add(this.lblSolicitudesModificacionRequerimientos);
+            this.Controls.Add(this.dgvSolicitudes);
+            this.Name = "frmSolicitudesModificacionRequerimientos";
+            this.Text = "frmSolicitudesModificacionRequerimientos";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvSolicitudes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvSolicitudes;
+        private System.Windows.Forms.Label lblSolicitudesModificacionRequerimientos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colProyecto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colEmpresa;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colFechaSolicitud;
+        private System.Windows.Forms.Button btnRevisarSolicitud;
+    }
+}
